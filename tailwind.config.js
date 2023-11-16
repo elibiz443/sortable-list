2350319f0adad3f7fa4f65cb62081682b3c1e4bf
elibiz443 <ellyambet10@gmail.com>
2023-11-16 21:09:41 +0300

    Added card modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,19 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-in": {
+          '0%': { opacity: '0%' },
+          '100%': { opacity: '100%' },
+        }
+      },
+      animation: {
+        "fade-in": 'fade-in 0.5s ease-in-out',
+      } 
+    }
+  }
 }
