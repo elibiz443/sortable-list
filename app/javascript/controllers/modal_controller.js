08ee9253fc332c488abe37d8e431f5da60706cd6
elibiz443 <ellyambet10@gmail.com>
2023-11-17 03:27:49 +0300

    Added card modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["modal", "form"]
+
+  open(event) {
+    event.preventDefault();
+    this.modalTarget.showModal();
+    this.modalTarget.addEventListener('click', (e) =>  this.backdropClick(e));
+  }
+
+  close(event) {
+    event.preventDefault();
+    this.modalTarget.close();
+    this.formTarget.reset();
+  }
+
+  backdropClick(event) {
+    event.target === this.modalTarget && this.close(event)
+  }
+}
